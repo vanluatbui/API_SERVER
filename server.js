adb0907b956360be61d2c1824a07cc336b58da06
Bui Van Luat <91833241+vanluatbui@users.noreply.github.com>
2022-03-10 22:33:27 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
-let express = require('express');
-let app = express();
-let port = process.env.PORT || 3000;
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+//require('dotenv').load()
+const port = process.env.PORT || 3000
 
-app.listen(port);
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
-console.log('RESTful API server started on: ' + port);
+let routes = require('./api/routes') //importing route
+routes(app)
+
+app.use(function(req, res) {
+    res.status(404).send({url: req.originalUrl + ' not found'})
+})
+
+app.listen(port)
+
+console.log('RESTful API server started on: ' + port)
